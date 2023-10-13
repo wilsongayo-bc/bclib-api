58982002efebeed52db588d1356e6fd79a11f82f
Alan C. Gabales <agabales69@gmail.com>
2023-10-13 10:26:01 +0800

    added course errors

---
+++ src/shared/errors/course/course.errors.ts
@@ -0,0 +1,19 @@
+export const CourseErrors = {
+    Conflict: {
+        statusCode: 409,
+        message: 'Name already exists',
+        error: "Conflict Error"
+    },
+
+    NotFound: {
+        statusCode: 404,
+        message: 'Publisher not exists',
+        error: "NotFound Error"
+    },
+
+    CourseNotFound: {
+        statusCode: 404,
+        message: 'Publisher not exists',
+        error: "NotFound Error"
+    },
+}
