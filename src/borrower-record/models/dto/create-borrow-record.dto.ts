841f64e47f5f91921ffc73c626b9679d8d7e42ac
Alan C. Gabales <agabales69@gmail.com>
2023-10-13 10:18:50 +0800

    added create borrow record

---
+++ src/borrower-record/models/dto/create-borrow-record.dto.ts
@@ -0,0 +1,40 @@
+import { IsEnum, IsNotEmpty, IsNotEmptyObject } from 'class-validator';
+import { BookStatus, ReturnStatus, Status, borrower_type } from 'src/enums/status.enum';
+import { Book, Course, Employee, Student } from 'src/typeorm';
+
+export class CreateBorrowRecordDto {
+    
+    @IsEnum(borrower_type)
+    borrower_type: borrower_type;
+    
+    @IsNotEmptyObject({ nullable: false })
+    student: Student
+
+    @IsNotEmptyObject({ nullable: false })
+    employee: Employee
+
+    @IsNotEmptyObject({ nullable: false })
+    book: Book
+
+    @IsNotEmpty()
+    date_borrowed: Date;
+
+    @IsNotEmpty()
+    date_returned: Date;
+
+    @IsNotEmpty()
+    remarks: string;
+
+    @IsNotEmpty()
+    Enrollment_date: Date;
+
+    @IsEnum(BookStatus)
+    book_status: BookStatus;
+
+    @IsEnum(ReturnStatus)
+    return_status: ReturnStatus;
+
+    created_by: string;
+
+    updated_by: string;
+}
