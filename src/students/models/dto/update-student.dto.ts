fb5be8e3252dbc8d82c31e8d213598988ae00c0d
Alan C. Gabales <agabales69@gmail.com>
2023-10-13 10:27:07 +0800

    added column

--- src/students/models/dto/update-student.dto.ts
+++ src/students/models/dto/update-student.dto.ts
@@ -1,10 +1,31 @@
-import { IsEnum, IsNotEmpty } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsNotEmptyObject } from 'class-validator';
 import { Status } from 'src/enums/status.enum';
+import { Course } from 'src/typeorm';
 
 export class UpdateStudentDto {
-    @IsNotEmpty()
-    name: string;
+    /*@IsNotEmpty()
+    name: string; */
 
+    @IsNotEmpty()
+    student_id: string;
+
+    @IsNotEmpty()
+    first_name: string;
+
+    @IsNotEmpty()
+    last_name: string;
+    s
+    full_name: string;
+
+    @IsNotEmptyObject({ nullable: false })
+    course: Course
+
+    @IsNotEmpty()
+    year_level: string;
+
+    @IsNotEmpty()
+    Enrollment_date: Date;
+    
     @IsEnum(Status)
     status: Status;
 
